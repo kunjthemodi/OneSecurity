22dcfd6da9a39adae9846d800d233e8b1422d751
KUNJKUMAR MODI <iamdev30@gmail.com>
2025-05-22 21:42:27 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ const credentialRoutes = require('./routes/credentialRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
